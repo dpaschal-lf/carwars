3da7855f7514f4e61912b689e61ecf4146a21c0f
John Carlisle <jmcarlisle@live.com>
2018-07-03 23:47:28 -0700

    added 3 cars and moved them to starting frid positions - John Carlisle

--- includes/main.js
+++ includes/main.js
@@ -4,7 +4,22 @@ var game;
 function initializeApp(){
 	game = new CarWarsEngine();
 	game.loadTrackInitiate('images/track_bounds.jpg');
-	game.loadCar( { driver: 'dan', number: 1});
+	game.loadCar( { driver: 'Dan', number: 43, color:"yellow",position: {
+		x: 400,
+		y: 100
+	},});
+	game.loadCar( { driver: 'John', number: 14, color: "lawngreen",position: {
+		x: 340,
+		y: 100
+	},});
+	game.loadCar( { driver: 'Rebecca', number: 13, color: "hotpink",position: {
+		x: 400,
+		y: 50
+	},});
+	game.loadCar( { driver: 'Federico', number: 99, color: "orangered",position: {
+		x: 340,
+		y: 50
+	},});
 	game.start();
 }
 
