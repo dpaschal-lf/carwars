bda6405936b72c26fea11acb8dfd18a628285994
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-09-13 12:39:34 -0700

    updated car color

--- includes/main.js
+++ includes/main.js
@@ -5,6 +5,7 @@ function initializeApp(){
 	game = new CarWarsEngine();
 	game.loadTrackInitiate('images/track_bounds.jpg');
 	game.loadCar( { driver: 'dan', number: 1});
+	game.loadCar( { driver: 'gerry', number: 13, position: {x:200, y:200}, color: 'pink'});
 	game.start();
 }
 
